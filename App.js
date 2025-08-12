056f16a3923ed35ce09205dec7bcdfcff6854851
viniolimpio3 <viniolimpio3@gmail.com>
2025-08-12 20:50:35 -0300

    Init

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Projeto - Cálculo de soma!</Text>
+      <Text> Digite primeiro número:</Text>
+
+      <TextInput />
+
+      <TouchableOpacity>
+        <Text>Somar</Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
